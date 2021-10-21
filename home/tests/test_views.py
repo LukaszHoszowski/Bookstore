8e2f347d64703bf255a1931c7412414d07627d90
Lukasz Hoszowski <hoszak@gmail.com>
2021-10-21 20:48:30 +0200

    pytest_completed

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -2,14 +2,28 @@ import pytest
 from django.urls import reverse
 
 
+@pytest.fixture
+def get_homepage_response(client):
+    url = reverse('home:home')
+    return client.get(url)
+
+
 def test_homepage_status_code(client):
     response = client.get('/')
 
     assert response.status_code == 200
 
 
-def test_homepage_status_code_reverse(client):
-    url = reverse('home:home')
-    response = client.get(url)
+def test_homepage_status_code_reverse(get_homepage_response):
 
-    assert response.status_code == 200
+    assert get_homepage_response.status_code == 200
+
+
+def test_homepage_template(get_homepage_response):
+
+    assert 'home/home.html' in [x.name for x in get_homepage_response.templates]
+
+
+def test_homepage_contains_correct_html(get_homepage_response):
+
+    assert 'Welcome to bookstore' in get_homepage_response.content.decode("UTF-8")
